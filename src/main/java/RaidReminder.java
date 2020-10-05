093b8a05018e113465b0dd3642fb8d77c5714e79
AgentRG <ron.gordin@gmail.com>
2020-10-04 19:23:12 -0700

    initial chance commit

--- src/main/java/RaidReminder.java
+++ src/main/java/RaidReminder.java
@@ -1,16 +1,13 @@
 import com.jagrosh.jdautilities.command.CommandClientBuilder;
 import com.jagrosh.jdautilities.commons.waiter.EventWaiter;
-import commands.Add;
 import commands.Start;
 import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.JDABuilder;
 import net.dv8tion.jda.api.OnlineStatus;
 import net.dv8tion.jda.api.entities.Activity;
 import org.apache.log4j.BasicConfigurator;
-import sqlite.SQLiteInitialize;
 
 import javax.security.auth.login.LoginException;
-import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
@@ -24,29 +21,19 @@ public class RaidReminder {
 
     public static void main (String[] args) throws IOException, LoginException, InterruptedException {
 
-        File database = new File(""); //Set path for the database
-
-        if (!database.exists()) {
-            new SQLiteInitialize()
-                    .newDatabase()
-                    .newTable();
-        }
-
-        List<String> configFile = Files.readAllLines(Paths.get("")); //Set path for the config file containing Discord credentials
+        List<String> configFile = Files.readAllLines(Paths.get("F:Database/config.txt")); //Set path for the config file containing Discord credentials
         BasicConfigurator.configure();
-        EventWaiter waiter = new EventWaiter();
+        var waiter = new EventWaiter();
 
         String token = configFile.get(0);
         String ownerId = configFile.get(1);
 
-        CommandClientBuilder client = new CommandClientBuilder()
+        var client = new CommandClientBuilder()
                 .setActivity(Activity.listening("rr.help"))
                 .setStatus(OnlineStatus.ONLINE)
                 .setOwnerId(ownerId)
                 .setPrefix("rr.")
-                .addCommands(
-                        new Add(),
-                        new Start());
+                .addCommands(new Start());
 
         JDABuilder.createDefault(token)
                 .addEventListeners(waiter, client.build())
--- src/main/java/commands/Start.java
+++ src/main/java/commands/Start.java
@@ -3,7 +3,6 @@ package commands;
 import com.jagrosh.jdautilities.command.Command;
 import com.jagrosh.jdautilities.command.CommandEvent;
 import net.dv8tion.jda.api.EmbedBuilder;
-import sqlite.SQLiteStart;
 import utilities.ReadyStrings;
 
 import java.awt.*;
@@ -11,41 +10,17 @@ import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.TimeZone;
-import java.util.Timer;
-import java.util.TimerTask;
 import java.util.concurrent.TimeUnit;
 
 public class Start extends Command {
 
-    private final String regex = "^rr.start [PTS]$";
+    private final String regex = "^rr.start (P|T|S|S2)$";
     private final ReadyStrings strings = new ReadyStrings();
-    private String sqlRaidValue;
+    private String raidValue;
 
     public Start() {
         this.name = "start";
-        this.help = "Start a raid reminder\n" +
-        "\n" +
-                "This Discord bot is based from UTC and uses a 24 hour cycle.\n" +
-                "\n" +
-                "All commands are preceded by:\n_P_  (The Pit)\n_T_  (Tank Takedown)\n_S_  (The Sith Triumvirate)\n" +
-                "\n" +
-                "Add command:\n" +
-                "_rr.add $ HH:MM MM_\n" +
-                "\n" +
-                "Start command:\n" +
-                "_rr.start $_" +
-                "\n" +
-                "\n" +
-                "Example:\n_rr.add S 16:00 10_ - Add a new raid reminder for _The Sith Triumvirate_, which starts at 4PM. Remind 10 minutes prior." +
-                "\n" +
-                "_rr.start S_ - Start the reminder set for _The Sith Triumvirate_." +
-                "\n" +
-                "\n" +
-                "Note: All reminders are saved locally. For future raids, just run the start command again." +
-                "\n" +
-                "\n" +
-                "For more help, please visit my GitHub page.\n" +
-                "https://github.com/AgentRG";
+        this.help = "Start a raid reminder";
     }
 
     /**
@@ -76,13 +51,13 @@ public class Start extends Command {
         String contentRaid = content.split(" ")[1];
         switch (contentRaid) {
             case "P":
-                sqlRaidValue = strings.thePitShortName();
+                raidValue = strings.thePitShortName();
                 break;
             case "T":
-                sqlRaidValue = strings.theTankTakedownShortName();
+                raidValue = strings.theTankTakedownShortName();
                 break;
             case "S":
-                sqlRaidValue = strings.theSithTriumvirateShortName();
+                raidValue = strings.theSithTriumvirateShortName();
                 break;
         }
     }
@@ -132,57 +107,7 @@ public class Start extends Command {
      */
     private void buildGoodStartReply(CommandEvent commandEvent, String channel, String content) {
         setRaidName(content);
-        String raidName = new SQLiteStart().startReminder(sqlRaidValue, channel)[0];
-        if (raidName.equals(strings.thePitShortName())) {
-            raidName = strings.thePitFullName();
-        } else if (raidName.equals(strings.theTankTakedownShortName())) {
-            raidName = strings.theTankTakedownFullName();
-        } else if (raidName.equals(strings.theSithTriumvirateShortName())) {
-            raidName = strings.theSithTriumvirateFullName();
-        }
-        if (new SQLiteStart().startReminder(sqlRaidValue, channel)[1] == null) {
-            commandEvent.reply(new EmbedBuilder()
-                    .setColor(new Color(172, 25, 25))
-                    .setTitle(strings.error())
-                    .setDescription(strings.noReminderSet())
-                    .build());
-        } else {
-            Timer timer = new Timer();
-            commandEvent.reply(new EmbedBuilder()
-                    .setColor(new Color(31, 161, 231))
-                    .setTitle(strings.success())
-                    .setDescription(strings.startingReminder())
-                    .build());
-            long reminder = Long.parseLong(new SQLiteStart()
-                    .startReminder(sqlRaidValue, channel)[2]
-                    .split(",")[1]);
-
-            //Starts timer here until the reminder is sent to the channel
-            String finalRaidName = raidName;
-            timer.schedule(new TimerTask() {
-                @Override
-                public void run() {
-                    String minuteOrMinutes;
-                    if (reminder == 1) {
-                        minuteOrMinutes = strings.minute();
-                    } else {
-                        minuteOrMinutes = strings.minutes();
-                    }
-                    long minuteDifference = 0;
-                    try {
-                        minuteDifference = calculateTimeDifference(getCurrentUtcTimeInString(), new SQLiteStart()
-                                .startReminder(sqlRaidValue, channel)[2]
-                                .split(",")[0] + ":00");
-                    } catch (ParseException e) {
-                        e.printStackTrace();
-                    }
-                    if (minuteDifference == reminder) {
-                        commandEvent.reply(strings.startReminderDescription(finalRaidName, reminder, minuteOrMinutes));
-                        timer.cancel();
-                    }
-                }
-            }, 0, 60000);
-        }
+        commandEvent.reply("oops");
     }
 
     /**
